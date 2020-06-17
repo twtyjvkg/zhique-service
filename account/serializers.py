31e7dbe3f477bda52b9156278c294e923c4bc3e1
xuzhao <xuzhao@zhique.design>
2020-06-17 19:39:17 +0800

    删除无用引用

--- account/serializers.py
+++ account/serializers.py
@@ -6,13 +6,9 @@ __email__ = 'xuzhao@zhique.design'
 
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework_jwt.settings import api_settings
 
 
 User = get_user_model()
-
-jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 LOGIN_TYPE = (
     ('account', '账户密码'),
